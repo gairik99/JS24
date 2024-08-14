7138fcb897f57783cc7f6c7e8045bb8e44bec1cd
gairik <gairikgmandal@gmail.com>
2024-08-14 11:48:18 +0530

    des

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello,World!')
+let arr=[1,14,2,5,9];
+function srt(a,b){
+    return a-b;
+}
+// arr.sort(srt);
+[a,b,...rest]=arr
+console.log(a);
+console.log(b);
+console.log(rest);
